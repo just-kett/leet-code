5baab52feae8bef3e6a40384a1abcce3fecd5f23
just-kett <kamne1212@gmail.com>
2026-09-23 01:53:47 +0700

    add solution for 3499_maximize_w_1_trade

---
+++ 3499_maximize_w_1_trade/sol_kett.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <string> 
+
+using namespace std;
+
+class Solution {
+public:
+
+    int maxActiveSectionsAfterTrade(string s) {   
+        int start = 0;
+        int length;
+        int max_length = 0;
+        int sum = 0;
+        vector<string> l;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[start] != s[i]) {
+                length = i - start;
+                string str = s.substr(start, length);
+                l.push_back(str);
+                start = i;
+            }
+        }
+        l.push_back(s.substr(start));
+        for (int j = 0; j < l.size(); j++) {
+            if (l[j][0] == '1' && j-1>=0 && stoi(l[j-1]) == 0 && j+1<l.size() && stoi(l[j+1]) == 0) {
+                length = l[j-1].length() + l[j+1].length();
+                int nstart = j-1;
+                int nend = j+1;
+                int nmid = j;
+                if (length > max_length) {
+                    max_length = length;
+                }
+            }
+        }
+        for (char j : s) {
+            if (j == '1') {
+                sum += 1;
+            }
+        }
+        return sum + max_length;
+    }
+};
